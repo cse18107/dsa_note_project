9ecae85913b102f00dd2cba647ae05e97b200e84
cse18107 <cse18107@cemk.ac.in>
2022-01-27 00:17:39 +0530

    adding navbar

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,41 +1,127 @@
-import React from 'react';
-import './HomePage.css';
+import React from "react";
+import "./HomePage.css";
 
-
-const Problem = [
-    {
-      name:"Reverse the array",
-      category:"Array",
-      solved:"true"
-    },  {
-      name:"Find the maximum and minimum element in an array",
-      category:"Array",
-      solved:"false"
-    },  {
-      name:"Find the Kth max and min element of an array",
-      category:"Array",
-      solved:"true"
-    },  {
-      name:"Move all the negative elements to one side of the array",
-      category:"Array",
-      solved:"true"
-    },  {
-      name:"level order traversal",
-      category:"Binary Tree",
-      solved:"true"
-    },  {
-      name:"Reverse level order traversal",
-      category:"Binary Tree",
-      solved:"true"
-    },  {
-      name:"Height of a tree",
-      category:"Binary Tree",
-      solved:"true"
-    },
-  ]
+const Problems = [
+  {
+    name: "Reverse the array",
+    category: "Array",
+    solved: "true",
+  },
+  {
+    name: "Find the maximum and minimum element in an array",
+    category: "Array",
+    solved: "false",
+  },
+  {
+    name: "Find the Kth max and min element of an array",
+    category: "Array",
+    solved: "true",
+  },
+  {
+    name: "Move all the negative elements to one side of the array",
+    category: "Array",
+    solved: "true",
+  },
+  {
+    name: "level order traversal",
+    category: "Binary Tree",
+    solved: "true",
+  },
+  {
+    name: "Reverse level order traversal",
+    category: "Binary Tree",
+    solved: "true",
+  },
+  {
+    name: "Height of a tree",
+    category: "Binary Tree",
+    solved: "true",
+  },  {
+    name: "Reverse the array",
+    category: "Array",
+    solved: "true",
+  },
+  {
+    name: "Find the maximum and minimum element in an array",
+    category: "Array",
+    solved: "false",
+  },
+  {
+    name: "Find the Kth max and min element of an array",
+    category: "Array",
+    solved: "true",
+  },
+  {
+    name: "Move all the negative elements to one side of the array",
+    category: "Array",
+    solved: "true",
+  },
+  {
+    name: "level order traversal",
+    category: "Binary Tree",
+    solved: "true",
+  },
+  {
+    name: "Reverse level order traversal",
+    category: "Binary Tree",
+    solved: "true",
+  },
+  {
+    name: "Height of a tree",
+    category: "Binary Tree",
+    solved: "true",
+  },
+];
 
 function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div className="homepage_body">
+      <div className="homepage_container">
+        <div className="homepage_content">
+          <div className="content_name">
+            <div className="id row">
+              <p>id</p>
+            </div>
+            <div className="name row">
+              <p>name</p>
+            </div>
+            <div className="category row">
+              <p>category</p>
+            </div>
+            <div className="status row">
+              <p>status</p>
+            </div>
+            <div className="solution row">
+              <p>solution</p>
+            </div>
+          </div>
+          <div className="page_content">
+            {Problems.map((problem,id)=>{
+                return (<div className="page_content--name">
+                <div className="id row">
+                  <p>{id+1}</p>
+                </div>
+                <div className="name row">
+                  <p>{problem.name}</p>
+                </div>
+                <div className="category row">
+                  <p>{problem.category}</p>
+                </div>
+                <div className="status row">
+                     <p>{problem.solved!=="true" && "not solved"}</p>
+                  <p>{problem.solved==="true" && "solved"}</p>
+                 
+                </div>
+                <div className="solution row">
+                  <p>solution</p>
+                </div>
+              </div>);
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
--- client/src/components/UI/NavBar.js
+++ client/src/components/UI/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './NavBar.css';
+import {NavLink} from 'react-router-dom';
+import logo from '../../image/programming.png';
+
 
 function NavBar() {
   return <div className="navbar_body">
@@ -7,13 +10,15 @@ function NavBar() {
           <div className="navbar_content">
             <div className="navbar_content--left">
                 <div className="navbar_content--left_content">
-                    <h3 className="navbar_content--left_content-logo">Dsa</h3>
+                    <img src={logo} className='navbar_logo' alt="DSA"/>
+                    <div className="navbar_content--left_content-logo">Dsa</div>
                 </div>
             </div>
             <div className="navbar_content--right">
                 <div className="navbar_content--right_content">
-                    <div className="home_page"></div>
-                    <div className="message_page"></div>
+                    <NavLink activeClassName="active" to="/" className="home_page">HOME</NavLink>
+                    <NavLink activeClassName="active" to="/add-question" className="add_page">ADD QUESTION</NavLink>
+                    <NavLink activeClassName="active" to="/login-signup" className="message_page">LOGIN/SIGN UP</NavLink>
                 </div>
             </div>
           </div>
